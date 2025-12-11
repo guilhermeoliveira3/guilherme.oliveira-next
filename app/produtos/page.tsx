a8754770c6899a8f4f9378190bd16dd585326284
guilhermeoliveira3 <a22409537@alunos.ulht.pt>
2025-12-11 12:19:50 +0000

    pesquisa em produtos

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -20,16 +20,43 @@ export default function page() {
 
     const { data, error, isLoading } = useSWR<Produto[]>(url, fetcher)
 
+    const [search, setSearch] = useState('')
+    const [filteredData, setFilteredData] = useState<Produto[]>([])
+
+    useEffect(() => {
+        if(!data) {
+            setFilteredData([])
+            return 
+        }
+
+        if (search.trim() === '') {
+            setFilteredData(data)   //se input vazio mostra tudo ;)
+        } else {
+            const filtrados = data.filter(product =>
+                product.title.toLowerCase().includes(search.toLowerCase())
+            )
+            setFilteredData(filtrados)
+        }
+    }, [search, data])
 
     if (error) return <p>error.message</p>
     if (isLoading) return <p>A descarregar dados</p>
     if (!data || data.length == 0) return <p>Não foi possível encontrar os produtos. Tente novamente!</p>
+
+
     return (
         <div className='flex flex-col justify-center items-center'>
 
             <h2 className='text-xl font-bold'>DeisiShop</h2>
+
+            <div className='w-full max-w-2xl mb-8'>
+                <input type="text" placeholder='Pesquisar produto...' value={search} onChange={(input) => setSearch(input.target.value)} 
+                className='w-full px-6 py-4 text-lg border border-gray-300 rounded-lg shadow focus:outline-none focus:ring-4 focus:ring-purple-300'/>
+
+            </div>
+
             <div className='grid grid-cols-3 gap-4 mt-2'>
-                {data?.map((product) => (
+                {filteredData?.map((product) => (
                     <ProductCard key={product.id} product={product}/>
                 ))}
             </div>
